f609f8054afba58281533240f85bb13118fe3a76
richadalal <65190493+richadalal@users.noreply.github.com>
2020-12-27 15:59:38 -0500

    Create CanadianSpelling.java

---
+++ CanadianSpelling.java
@@ -0,0 +1,28 @@
+//Richa Dalal 
+//Program Which Converts American Spelling to Canadian Spelling
+ 
+import hsa.Console; 
+import java.awt.*; 
+import java.util.*; 
+import javax.imageio.*; 
+import java.io.*; 
+ 
+public class CanadianSpelling 
+  
+{
+static Console c; 
+
+ 
+  public static void main(String[] args) throws IOException  
+  {
+    c = new Console(); 
+    
+    String word; 
+    c.println("Type any word which you would like to see a change in spelling for");  
+    word = c.readLine(); 
+   String replaced = word.replace("or", "our"); 
+   c.println("Here is the new word with the Canadian spelling:"); 
+   c.println(replaced); 
+     
+  }
+}
